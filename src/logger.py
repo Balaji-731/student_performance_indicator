1e2705a1ca4333475ff753138664d4e3a6de2f72
Balaji <poralabalaji@gmail.com>
2025-11-26 17:29:40 +0530

    logging

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 import datetime
 
 LOG_FILE=f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path=os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
@@ -13,3 +13,6 @@ logging.basicConfig(
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+if __name__=="__main__":
+    logging.info("Logging setup complete.")
